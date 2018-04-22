e4911a8fc7353237e77626435e2da8dfc67adc83
SiaSyPeng <shiyao.peng.20@dartmouth.edu>
2018-04-21 21:49:52 -0400

    add all plugins

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+// const $ = require('jquery');
+
 console.log('starting up!');
+
+// $('#main').html("You've been on this page for ${num} seconds.");
+let num = 0;
+
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+  num += 1;
+}, 1000);
